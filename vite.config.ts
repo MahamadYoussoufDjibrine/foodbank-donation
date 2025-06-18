761587b44f83a7d1072b8e08ab3f8eabea8907a6
Mahamad Youssouf Djibrine <mhtoka723@gmail.com>
2025-06-18 10:51:21 +0200

    Update source code after build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 600, // increase limit to reduce warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // put all node_modules code into a separate vendor chunk
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
